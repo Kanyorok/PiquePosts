c28a1234204b534f3c8a8f88008822b6a4a7358e
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-12-02 05:49:20 +0300

    Change the defaults for comment and post counters to zero

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id', counter_cache: true
   has_many :comments
   has_many :likes
-  validates :Title, presence: true, length: { maximum: 250 }
-  validates :likes_count, presence: false, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :comments_count, presence: false, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :likes_counter, presence: false, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, presence: false, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def mostrecentcomments
     omments.where(post_id: self[:id]).order(created_at: :desc).limit(5)
