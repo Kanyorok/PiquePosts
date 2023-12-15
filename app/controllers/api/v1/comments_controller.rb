c2605f126564a9316d52e213b6265f9762c5fe83
Al-Amin <alaminkhanshakil@gmail.com>
2023-12-15 19:47:15 +0600

    Create update and destroy comment

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -7,4 +7,27 @@ class Api::V1::CommentsController < ApplicationController
   def show
     render json: @comment
   end
+
+  def create
+    @post = post
+    @comment = @post.comments.new(comment_params)
+
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @comment.destroy
+  end
 end
