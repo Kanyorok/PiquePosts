a9f9b2a68f516a5b14bbe7771284d283c7fa7305
Al-Amin <alaminkhanshakil@gmail.com>
2023-12-15 14:29:47 +0600

    Add default

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,15 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  after_initialize :set_default_role, if: :new_record?
+
   def recentposts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  private
+
+  def set_default_role
+    self.role ||= :user
   end
 end
