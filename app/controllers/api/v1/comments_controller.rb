f1e384fc1652c60c61746ddfeb4893778d2d9f07
Al-Amin <alaminkhanshakil@gmail.com>
2023-12-15 19:45:37 +0600

    Show the comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::CommentsController < ApplicationController
+  def index
+    @comments = Comment.where(post_id: post.id)
+    render json: @comments
+  end
+
+  def show
+    render json: @comment
+  end
 end
